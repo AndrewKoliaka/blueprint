e678e70b24df7af1800165f20234357a7f8d66ad
AndrewKoliaka <andrew.koliaka@gmail.com>
2017-03-04 00:13:00 +0200

    created base files & folders structure

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const connect = require('gulp-connect');
+
+const pathTo = {
+    less: 'src/assets/less/**/*.less',
+    js: 'src/js/*.js',
+    css: 'src/assets/css/*.css'
+}
+
+gulp.task('connect', () => {
+    connect.server({
+        root: './',
+        port: 3000,
+        livereload: true
+    });
+});
+
+gulp.task('less', () => {
+    gulp.src(pathTo.less)
+        .pipe(concat('styles.css'))
+        .pipe(less())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('src/assets/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', () => {
+    gulp.src('src/js/*.js')
+        .pipe(connect.reload());
+});
+
+gulp.task('html', () => {
+    gulp.src('index.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', () => {
+    gulp.watch(pathTo.less, ['less']);
+    gulp.watch(pathTo.js, ['js']);
+    gulp.watch('./index.html', ['html']);
+});
+
+gulp.task('default', ['less', 'connect', 'watch']);
