1bde63e3d96efcf6fc134699e2dab635b7ef4a07
AndrewKoliaka <andrew.koliaka@gmail.com>
2017-07-21 23:32:03 +0300

    added dist & bugs fixed

--- gulpfile.js
+++ gulpfile.js
@@ -3,45 +3,78 @@ const less = require('gulp-less');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const connect = require('gulp-connect');
+const imagemin = require('gulp-imagemin');
+const merge = require('merge-stream');
+const del = require('del');
 
 const pathTo = {
+    index: './index.html',
     less: 'src/assets/less/**/*.less',
     js: 'src/js/*.js',
-    css: 'src/assets/css/*.css'
+    css: 'src/assets/css/*.css',
+    images: 'src/assets/images/**/*'
 }
 
 gulp.task('connect', () => {
-    connect.server({
-        root: './',
-        port: 3000,
-        livereload: true
-    });
+    connect.server({root: './', port: 3000, livereload: true});
 });
 
 gulp.task('less', () => {
-    gulp.src(pathTo.less)
+    gulp
+        .src(pathTo.less)
         .pipe(concat('styles.css'))
         .pipe(less())
         .pipe(autoprefixer())
-        .pipe(gulp.dest('src/assets/css'))
+        .pipe(gulp.dest('dist/css/'))
         .pipe(connect.reload());
 });
 
 gulp.task('js', () => {
-    gulp.src('src/js/*.js')
+    gulp
+        .src(pathTo.js)
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('dist/js/'))
         .pipe(connect.reload());
 });
 
+gulp.task('libs', () => {
+    gulp
+        .src('src/assets/libs/**')
+        .pipe(gulp.dest('dist/libs/'));
+});
+
 gulp.task('html', () => {
-    gulp.src('index.html')
+    gulp
+        .src(pathTo.index)
         .pipe(connect.reload());
+});
+
+gulp.task('assets', () => {
+    let images = gulp
+        .src(pathTo.images)
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images/'));
+
+    let fonts = gulp
+        .src('src/assets/fonts/**')
+        .pipe(gulp.dest('dist/fonts/'));
+
+    let videos = gulp
+        .src('src/assets/videos/*')
+        .pipe(gulp.dest('dist/videos/'));
+
+    return merge(images, fonts, videos);
+});
+
+gulp.task('del', () => {
+    del(['dist']);
 });
 
 gulp.task('watch', () => {
     gulp.watch(pathTo.less, ['less']);
     gulp.watch(pathTo.js, ['js']);
-    gulp.watch('./index.html', ['html']);
+    gulp.watch(pathTo.index, ['html']);
 });
 
 gulp.task('default', ['less', 'connect', 'watch']);
-gulp.task('build', ['less', 'js']);
+gulp.task('build', ['less', 'js', 'assets', 'libs']);
--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,57 +1,64 @@
- var carousel = (function () {
-     var owl, index = 0,
-         last = 0;
-     var iconNamesWhite = ['icon-cube-w', 'icon-speedometer-w', 'icon-line-chart-w'],
-         iconNamesBlack = ['icon-cube', 'icon-speedometer', 'icon-line-chart'];
-     var isScrollAllowed = true;
+var carousel = (function () {
+    var owl,
+        index = 0,
+        last = 0;
+    var iconNamesWhite = [
+            'icon-cube-w', 'icon-speedometer-w', 'icon-line-chart-w'
+        ],
+        iconNamesBlack = ['icon-cube', 'icon-speedometer', 'icon-line-chart'];
+    var isScrollAllowed = true;
 
-     return {
-         init: function () {
-             var self = this;
-             owl = $('.owl-carousel');
-             owl.owlCarousel({
-                 items: 1,
-                 loop: true,
-                 autoplay: true,
-                 autoplayTimeout: 5000
-             });
-             owl.on('changed.owl.carousel', function (event) {
-                 var index = (event.item.index - 2) < 0 ? 2 : event.item.index - 2;
-                 var icon = $('.card-icon__wrapper').get(index);
+    return {
+        init: function () {
+            var self = this;
+            owl = $('.owl-carousel');
+            owl.owlCarousel({items: 1, loop: true, autoplay: true, autoplayTimeout: 5000});
+            owl.on('changed.owl.carousel', function (event) {
+                var index = (event.item.index - 2) < 0
+                    ? 2
+                    : event.item.index - 2;
+                var icon = $('.card__icon-wrapper').get(index);
 
-                 $($('.card-icon__wrapper').get(last)).children().removeClass().addClass(iconNamesBlack[last]);
-                 $('.card-icon__wrapper').removeClass('card__icon-wrapper--active');
-                 $(icon).addClass('card__icon-wrapper--active');
-                 $(icon).children().addClass(iconNamesWhite[index]);
-                 last = index;
-             });
-             $('.carousel-btn--prev').click(this.prevSlide);
-             $('.carousel-btn--next').click(this.nextSlide);
-             $('.btn--expand').click(this.expand);
+                $($('.card__icon-wrapper').get(last))
+                    .children()
+                    .removeClass(iconNamesWhite[last])
+                    .addClass(iconNamesBlack[last]);
+                $('.card__icon-wrapper').removeClass('card__icon-wrapper--active');
+                $(icon).addClass('card__icon-wrapper--active');
+                $(icon)
+                    .children()
+                    .addClass(iconNamesWhite[index]);
+                last = index;
+            });
+            $('.carousel-btn--prev').click(this.prevSlide);
+            $('.carousel-btn--next').click(this.nextSlide);
+            $('.btn--expand').click(this.expand);
 
-             //  if slide in full screen - disable scrolling
-             $('body').on('mousewheel', function (event) {
-                 if (!isScrollAllowed) {
-                     event.preventDefault();
-                 }
-             });
-         },
-         nextSlide: function () {
-             owl.trigger('next.owl.carousel');
-         },
-         prevSlide: function () {
-             owl.trigger('prev.owl.carousel');
-         },
-         expand: function (event) {
-             if ($('.slide--expanded').length === 1) {
-                 return false;
-             }
-             isScrollAllowed = false;
-             var slide = $(this).parent().parent();
-             var img = $(slide.children().get(0));
-             var caption = $(slide.children().get(1));
+            //  if slide in full screen - disable scrolling
+            $('body').on('mousewheel', function (event) {
+                if (!isScrollAllowed) {
+                    event.preventDefault();
+                }
+            });
+        },
+        nextSlide: function () {
+            owl.trigger('next.owl.carousel');
+        },
+        prevSlide: function () {
+            owl.trigger('prev.owl.carousel');
+        },
+        expand: function (event) {
+            if ($('.slide--expanded').length === 1) {
+                return false;
+            }
+            isScrollAllowed = false;
+            var slide = $(this)
+                .parent()
+                .parent();
+            var img = $(slide.children().get(0));
+            var caption = $(slide.children().get(1));
 
-             var expadedSlide = $(`
+            var expadedSlide = $(`
             <div class="slide slide--expanded">
                 <span class="btn btn--close">
                     <i class="fa fa-times" aria-hidden="true"></i>
@@ -63,12 +70,12 @@
                     </figcaption>
                 </figure>
             </div>`).appendTo(document.body);
-             $('.btn--close').click(() => {
-                 $('.slide--expanded').remove();
-                 owl.trigger('play.owl.autoplay', [1000]);
-                 isScrollAllowed = true;
-             });
-             owl.trigger('stop.owl.autoplay');
-         }
-     }
- })();
+            $('.btn--close').click(() => {
+                $('.slide--expanded').remove();
+                owl.trigger('play.owl.autoplay', [1000]);
+                isScrollAllowed = true;
+            });
+            owl.trigger('stop.owl.autoplay');
+        }
+    }
+})();
